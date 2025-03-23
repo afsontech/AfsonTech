500a757aef7e4743e52cb566fac5590a67e0478c
afson <afsontech@gmail.com>
2025-03-23 22:39:51 +0500

    change in setting

--- afsontech_web/afsontech_web/settings.py
+++ afsontech_web/afsontech_web/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8m3z$r*!hr7y+)vxo^xsg^7kht-ec#kx(7dyl9towit6a_9z^2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
